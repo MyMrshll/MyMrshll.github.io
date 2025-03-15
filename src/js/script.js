1a6432db43773fdd824954694f19b30abe31e54c
Ferta <fertaprogram@gmail.com>
2025-03-15 23:38:26 +0700

    two

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const navbar = document.querySelector('.navbar-nav')
+const hamburger = document.getElementById('hamburger') 
+const background = document.getElementById('background')
+const btnIG = document.getElementById('btn-ig')
+
+console.log(hamburger)
+addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbar.contains(e.target)) {
+        navbar.classList.remove('active')
+    }
+})
+
+btnIG.addEventListener('click', () =>{
+    const url = 'https://www.instagram.com/fertanindi/'
+    window.location.href = url, '_blank'
+})
+
+hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('active')
+})
